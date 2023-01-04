d01c90afcefd48a46233e5f80a18fcdd7ec5e742
AKSHANSH CHAHANDE <113416935+Akshansh789@users.noreply.github.com>
2023-01-04 11:14:29 +0530

    task 3

---
+++ dom.js
@@ -0,0 +1,10 @@
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+
+header.style.borderBottom= 'solid 8px  black';
+
+
+var item = document.getElementById('list-group-item');
+console.log(item);
+item.style.fontWeight = 'bold';
+item.style.backgroundColor = 'green';
